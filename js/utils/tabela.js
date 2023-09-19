99d68d86d5f268139b4f91c1b556e12f3b15cc3a
André devkcud Albanese <patommmmm@proton.me>
2023-09-19 19:46:08 -0300

    clean: Limpeza no código

--- js/utils/tabela.js
+++ js/utils/tabela.js
@@ -1,76 +1,44 @@
 class Tabela {
     #elementoTabela = document.querySelector('table');
 
-    #criarLinha(celulas) {
-        const row = this.#elementoTabela.insertRow();
+    #criarLinha(...elementos) {
+        const linha = this.#elementoTabela.insertRow();
 
-        for (const texto of celulas) {
-            row.insertCell().textContent = texto;
+        for (let elemento of elementos) {
+            const celula = linha.insertCell();
+
+            if (typeof elemento !== 'object') {
+                elemento = document.createTextNode(elemento);
+            }
+
+            celula.appendChild(elemento);
+            linha.appendChild(celula);
         }
-
-        return row;
-    }
-
-    #criarCelulaEstoque(produto) {
-        const celula = this.#elementoTabela.insertRow();
-
-        const botaoDiminuir = document.createElement('button');
-        botaoDiminuir.textContent = '-';
-        botaoDiminuir.addEventListener('click', () => {
-            estoque.atualizar(produto.id, produto.nome, parseInt(produto.quantidade) - 1, produto.precoUnitario);
-
-            Storage.inventario = estoque.produtos;
-            this.atualizar();
-        });
-
-        const botaoAumentar = document.createElement('button');
-        botaoAumentar.textContent = '+';
-        botaoAumentar.addEventListener('click', () => {
-            estoque.atualizar(produto.id, produto.nome, parseInt(produto.quantidade) + 1, produto.precoUnitario);
-
-            Storage.inventario = estoque.produtos;
-            this.atualizar();
-        });
-
-        botaoDiminuir.style.marginRight = '10px';
-        botaoAumentar.style.marginLeft = '10px';
-
-        celula.appendChild(botaoDiminuir);
-        celula.appendChild(document.createTextNode(produto.quantidade));
-        celula.appendChild(botaoAumentar);
-
-        return celula;
     }
 
     atualizar() {
         this.#elementoTabela.innerHTML = '';
-        this.#criarLinha([
+        this.#criarLinha(
             'ID',
             'Nome',
             'Quantidade',
             'Preço Unitário',
             'Preço Total',
-        ]);
+        );
 
         for (const produto of Storage.inventario) {
-            const linha = this.#criarLinha([produto.id, produto.nome]);
-
-            linha.insertCell().appendChild(this.#criarCelulaEstoque(produto));
-
-            linha.insertCell().textContent = `R$${parseFloat(
-                produto.precoUnitario,
-            ).toFixed(2)}`;
-            linha.insertCell().textContent = `R$${(
-                produto.quantidade * produto.precoUnitario
-            ).toFixed(2)}`;
+            this.#criarLinha(
+                produto.id,
+                produto.nome,
+                produto.quantidade,
+                `R$${parseFloat(produto.precoUnitario).toFixed(2)}`,
+                `R$${(produto.quantidade * produto.precoUnitario).toFixed(2)}`,
+            );
         }
 
-        const total = Storage.inventario.reduce(function (
-            valor,
-            { precoUnitario, quantidade },
-        ) {
-            return valor + precoUnitario * quantidade;
+        const total = Storage.inventario.reduce(function (valor, prod) {
+            return valor + prod.precoUnitario * prod.quantidade;
         }, 0);
-        this.#criarLinha(['', '', '', '', `R$${total.toFixed(2)}`]);
+        this.#criarLinha('', '', '', '', `R$${total.toFixed(2)}`);
     }
 }
